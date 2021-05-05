272085eaa895d64d848d060f4d560c995574063e
Oliver N <72499511+NemesOliver@users.noreply.github.com>
2021-05-05 20:54:35 +0100

    Card component

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,12 @@
-import React, { useEffect } from "react";
-import { connect } from "react-redux";
+import React from "react";
+import ChartCard from "./charts/ChartCard";
 
-import { fetchTotal } from "../actions";
-
-const App = ({ fetchTotal, total }) => {
-  useEffect(() => {
-    fetchTotal();
-  }, [fetchTotal]);
-
-  const renderList = () => {
-    if (!total[0]) {
-      return <div>Loading...</div>;
-    }
-
-    return (
-      <>
-        <div className="item">Date: {total[0].last_update} </div>
-        <div className="item">
-          Confirmed cases: {total[0].confirmed.toLocaleString()}
-        </div>
-        <div className="item">Deaths: {total[0].deaths.toLocaleString()}</div>
-        <div className="item">
-          Mortality rate: {total[0].fatality_rate.toLocaleString()}%
-        </div>
-      </>
-    );
-  };
-
+const App = () => {
   return (
     <div className="ui container">
-      <div className="ui list">{renderList()}</div>
+      <ChartCard />
     </div>
   );
 };
 
-const mapStateToProps = (state) => {
-  return { total: state.total };
-};
-
-export default connect(mapStateToProps, {
-  fetchTotal,
-})(App);
+export default App;
---
+++ src/components/charts/ChartCard.js
@@ -0,0 +1,54 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchTotal } from "../../actions";
+
+const ChartCard = ({ fetchTotal, total }) => {
+  useEffect(() => {
+    fetchTotal();
+  }, [fetchTotal]);
+
+  const renderList = () => {
+    if (!total[0]) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <>
+        <div className="item">Date: {total[0].last_update} </div>
+        <div className="item">
+          Confirmed cases: {total[0].confirmed.toLocaleString()}
+        </div>
+        <div className="item">Deaths: {total[0].deaths.toLocaleString()}</div>
+        <div className="item">
+          Mortality rate: {total[0].fatality_rate.toLocaleString()}%
+        </div>
+      </>
+    );
+  };
+  return (
+    <div className="ui raised fluid card">
+      <div className="content">
+        <div className="header">Cute Dog</div>
+        <div className="meta">
+          <span className="category">Animals</span>
+        </div>
+        <div className="description">
+          <div className="ui list">{renderList()}</div>
+        </div>
+      </div>
+      <div className="extra content">
+        <div className="right floated author">
+          <i className="image icon"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return { total: state.total };
+};
+
+export default connect(mapStateToProps, {
+  fetchTotal,
+})(ChartCard);
